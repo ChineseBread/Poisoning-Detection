526cf3bd89ffeb618a0f4eab85fe9ee3a732a364
zhangyixuan <zhangyixuan@xiaohongshu.com>
2023-01-26 19:58:51 +0800

    feat: demo

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,15 +1,25 @@
 import React, {Fragment} from 'react';
-import Center from "@/components/Home/Center";
-function Index() {
+import CenterDotMap from "@/components/Home/CenterDotMap";
+import RiskWordCloud from "@/components/Home/RiskWordCloud";
+import DemoLine from "@/components/Home/DemoLine";
+
+function Home() {
   return (
     <Fragment>
       <div className='flex-col'></div>
       <div className='flex-center'>
-        <Center/>
+        <CenterDotMap/>
+        <div className='col-line-container'>
+          <DemoLine/>
+          <DemoLine/>
+        </div>
       </div>
-      <div className='flex-col'></div>
+      <div className='flex-col'>
+        <RiskWordCloud/>
+
+      </div>
     </Fragment>
   );
 }
 
-export default Index;
+export default Home
--- src/config/mock.config.ts
+++
@@ -1,1 +0,0 @@
-export {default as FlylineData} from '@/mock/Flyline'
